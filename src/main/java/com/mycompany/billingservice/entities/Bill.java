15b744890b8569cceba406c894f0a1ba5a309edf
jackamrl <jacquesboyodi@yahoo.fr>
2022-11-02 12:52:52 +0000

    billItem start

--- src/main/java/com/mycompany/billingservice/entities/Bill.java
+++ src/main/java/com/mycompany/billingservice/entities/Bill.java
@@ -1,13 +1,21 @@
 package com.mycompany.billingservice.entities;
 
+import com.sun.istack.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
+
+// JackDev
+//update 02/11/2022
 
 import javax.persistence.*;
 import java.util.Date;
+import java.util.List;
+
+
+//The entity of bill
+//It have relation with Quote, BillItem, Patient and Setlement
 
 @Entity
 @Table(name = "bill")
@@ -15,10 +23,24 @@ import java.util.Date;
 public class Bill {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "no_bill")
     private Integer noBill;
+    @Basic(optional = false)
+    @Column(name = "bill_date")
     private Date billDate;
+    @Basic(optional = false)
+    @Column(name = "validation_date")
     private Date validationDate;
+    @Basic()
+    @Column(name = "discount_percentage")
     private Double discountPercentage;
+    @Basic(optional = false)
+    @Column(name = "total_ht")
     private Double totalHt;
+    @Basic(optional = false)
+    @Column(name = "total_ttc")
     private Double totalTTC;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "bill")
+    private List<BillItem> billItemMap;
 }
--- src/main/java/com/mycompany/billingservice/entities/BillItem.java
+++ src/main/java/com/mycompany/billingservice/entities/BillItem.java
@@ -1,10 +1,43 @@
 package com.mycompany.billingservice.entities;
 
-public class BillItem {
 
+import com.sun.istack.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+// JackDev
+//update 02/11/2022
+
+import javax.persistence.*;
+
+//The entity of billItem
+//It have relation with Bill and Product
+
+@Entity
+@Table(name = "bill_item")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class BillItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Integer id;
+    @Basic(optional = false)
+    @Column(name = "qte")
     private Integer qte;
+    @Basic(optional = false)
+    @Column(name = "amount")
     private Double amount;
+    @Basic()
+    @Column(name = "rate_tva")
     private Double rateTva;
+    @Basic(optional = false)
+    @Column(name = "discount_percentage")
     private Double discountPercentage;
+    @ManyToOne(optional = false, fetch = FetchType.EAGER)
+    private Bill bill;
 }
